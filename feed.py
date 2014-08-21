d7b683de4fe39bd4ad5ff1646f3e328735fa1464
egoing <egoing@gmail.com>
2014-08-22 01:05:58 +0900

    * 홈페이지에서 리스트 출력하도록 변경

--- feed.py
+++ feed.py
@@ -37,6 +37,8 @@ Using JavaScript is recommended if it is feasible for your application,
 as it handles some complex authentication states that can only be detected
 in client-side code.
 """
+from facebook import Graph
+from controller import *
 
 import webapp2
 import jinja2
@@ -61,6 +63,10 @@ from google.appengine.ext import ndb
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp import util
 from google.appengine.ext.webapp import template
+from model import *
+from cookie import *
+
+
 
 import time
 import re
@@ -71,175 +77,47 @@ from google.appengine.api import lib_config
 
 import logging
 
-import sys
-logging.getLogger().setLevel(logging.DEBUG)
-reload(sys)
-sys.setdefaultencoding('utf-8')
-
-_config = lib_config.register('main', {'FACEBOOK_ID':None, 'FACEBOOK_SECRET':None, 'FQL_ACCESS_TOKEN':None, 'GROUP_ID':None})
-
-#페이스북 앱 정보
-
-FACEBOOK_APP_ID = _config.FACEBOOK_ID
-FACEBOOK_SECRET = _config.FACEBOOK_SECRET
-FQL_ACCESS_TOKEN = _config.FQL_ACCESS_TOKEN
-GROUP_ID = _config.GROUP_ID
-
-config = {}
-config['webapp2_extras.sessions'] = dict(secret_key='')
 
 
-# dao 모델 설정
-class User(ndb.Model):
-    id = ndb.StringProperty(required=True)
-    created = ndb.DateTimeProperty(auto_now_add=True)
-    updated = ndb.DateTimeProperty(auto_now=True)
-    name = ndb.StringProperty(required=True)
-    profile_url = ndb.StringProperty(required=True)
-    access_token = ndb.StringProperty(required=True)
-
-class Config(ndb.Model):
-    key = ndb.StringProperty(required=True)
-    value = ndb.TextProperty(required=True)
-
-class Feed(ndb.Model):
-    id = ndb.StringProperty(required=True)
-    message = ndb.TextProperty(required=True)
-    full_picture = ndb.StringProperty()
-    created_time = ndb.StringProperty(required=True)
-    updated_time = ndb.StringProperty(required=True)
-    link = ndb.StringProperty()
-
-#템플릿 프레임워크 환경 설정
-
+# 템플릿 프레임워크 환경 설정
 JINJA_ENVIRONMENT = jinja2.Environment(
-  loader = jinja2.FileSystemLoader(os.path.dirname(__file__)),
-  extensions = ['jinja2.ext.autoescape'])
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'])
 
-class BaseHandler(webapp.RequestHandler):
-    @property
-    def current_user(self):
-        """Returns the logged in Facebook user, or None if unconnected.
-        페이스북 유저가 로그인되어 있으면 페이스북 객체를 리턴한고 그렇지 않은면 None를 리턴함
-        """
-        logging.info("순서 1")
-        if not hasattr(self, "_current_user"):
-            self._current_user = None
-            user_id = parse_cookie(self.request.cookies.get("fb_user"))
-            if user_id:
-                self._current_user = User.get_by_id(user_id)
-        return self._current_user
+# 메인 클래스
 
 
-
-#메인 클래스
 class HomeHandler(BaseHandler):
+
     def get(self):
         logging.info("순서 먼저 호")
         args = dict(current_user=self.current_user)
         template = JINJA_ENVIRONMENT.get_template('/view/oauth.html')
-        self.response.write(template.render(args))
+        feeds = Feed.query().fetch()
+        self.response.write(template.render({'feeds': feeds}))
 
 
-#로그인 담당
+# 로그인 담당
 class LoginHandler(BaseHandler):
+
     def get(self):
-        verification_code = self.request.get("code")
-        args = dict(client_id=FACEBOOK_APP_ID,
-                    redirect_uri=self.request.path_url)
-        if self.request.get("code"):
-            args["client_secret"] = FACEBOOK_SECRET
-            args["code"] = self.request.get("code")
-            response = cgi.parse_qs(urllib.urlopen(
-                "https://graph.facebook.com/oauth/access_token?" +
-                urllib.urlencode(args)).read())
-            #엑시스 토근을 요청
-            access_token = response["access_token"][-1]
-
-            # Download the user profile 사용자 프로파이일을 다운로드하고 
-            # and cache a local instance of the 로컬 DAO에 프로필정보를 담는다.
-            # basic profile info
-            profile = json.load(urllib.urlopen("https://graph.facebook.com/me?" + urllib.urlencode(dict(access_token=access_token))))
-            
-            #logging.info(urllib.urlencode("debeg" + urllib.urlencode(dict(access_token=access_token)))
-            
-            logging.info("https://graph.facebook.com/me?" + urllib.urlencode(dict(access_token=access_token)))
-
-
-            logging.info("after call profile ")
-            user = User(key_name=str(profile["id"]), id=str(profile["id"]),
-                        name=profile["name"], access_token=access_token,
-                        profile_url=profile["link"])
-            user.put()
-            set_cookie(self.response, "fb_user", str(profile["id"]), expires=time.time() + 30 * 86400)
-            self.redirect("/")
-        else:
-            self.redirect(
-                "https://graph.facebook.com/oauth/authorize?" + urllib.urlencode(args))
+        graph = Graph()
+        graph.login(self)
 
 
 class LogoutHandler(BaseHandler):
+
     def get(self):
         set_cookie(self.response, "fb_user", "", expires=time.time() - 86400)
         self.redirect("/")
 
 
-"""그룹에 글을 가져온다. FQL를 이용한다. FQL은 페이스북 API v2.1에서 디플리케션 되었다. 신규로 등록된 어플리케이션 그 이전 버전의 API를 사용하지 못한다.
-자세한 정보 : 
-1. http://stackoverflow.com/questions/25256428/query-facebook-for-what-version-of-the-graph-api-is-being-used-can-be-used
-2. https://developers.facebook.com/docs/apps/changelog
-"""
-class GroupsFqlHandler(BaseHandler):
-    def get(self):
-        user = self.current_user
-        self.response.write("fql groups post")
-        self.response.write(user.name)
-        if user:
-            query = "SELECT post_id FROM stream WHERE source_id='"+ GROUP_ID +"' LIMIT 10"
-            fql = {'q': "SELECT post_id FROM stream WHERE source_id='"+ GROUP_ID +"' LIMIT 10"}
-            fql1 = {'q': "SELECT message, message_tags FROM stream WHERE source_id='1389107971348349' LIMIT 10"}
-
-            args = "https://graph.facebook.com/v2.0/fql?access_token=" + FQL_ACCESS_TOKEN + "&" +urllib.urlencode(fql1) +"&format=json"
-            #logging.info(args)
-            file = urllib2.urlopen(args, None, timeout=5000)
-
-            import unicodedata
-
-            try:
-                content = file.read()
-                #logging.info(content)
-                self.response.write(" <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\" /><br />")
-
-                stringUnicode = '\ud504\ub85c\uadf8\ub798\ubc0d\uacfc'
-                #uni = stringUnicode.decode("UTF-8")
-                self.response.write('<script>'+'console.log(unescape("'+uni.encode('utf-8')+'"))</script>')
-                self.response.write('<script>'+'document.write(unescape("'+uni.encode('utf-8')+'"))</script>')
-                self.response.write(uni.replace('%u',r'\u').decode("unicode_escape"))    
-            except Exception, e:
-                raise e
-            finally:
-                file.close()
-        else:
-            self.response.write("2")
-
-
 class GroupsGraphApiHandler(BaseHandler):
 
-    def get(self) :
-        from time import strptime, strftime     
-        #iself.response.write("groupsGraphAPI Call")
-        url = "https://graph.facebook.com/" + GROUP_ID + "?fields=feed.limit(10){message,full_picture,created_time,updated_time,id,link}&method=GET&format=json&suppress_http_code=1&access_token=" + str(User.query().get().access_token)
-        graphApi = url
-        logging.info(url)
-        file = urllib2.urlopen(graphApi)
-        content = json.loads(file.read())
-
-        #에러 처리
-        if "error" in content:
-            if content["error"]["code"] == 613:
-                time.sleep(200)
-                file = urllib2.urlopen("https://graph.facebook.com/" + GROUP_ID + "?feed.limit(1){message,full_picture,created_time,created_time,id,link}&method=GET&format=json&suppress_http_code=1&access_token=" + str(token))
-                content = json.loads(file.read())
+    def get(self):
+        from time import strptime, strftime
+        graph = Graph()
+        content = graph.groups()
 
         previous = content["feed"]["paging"]["next"]
         NewsFeed = content["feed"]
@@ -247,125 +125,72 @@ class GroupsGraphApiHandler(BaseHandler):
         logging.info("previous : " + previous)
 
         NewsFeedMessage = '<b>message</b>'
-        config = Config.query(Config.key == 'last_synced_time').get();
+        config = Config.query(Config.key == 'last_synced_time').get()
         if config:
             last_synced_time = strptime(config.value, "%Y-%m-%dT%H:%M:%S+0000")
         else:
             last_synced_time = strptime("1979", "%Y")
 
-        max_created_time = last_synced_time;
+        max_created_time = last_synced_time
 
         for row in content["feed"]["data"]:
-            entry_created_time = strptime(row['created_time'], "%Y-%m-%dT%H:%M:%S+0000")
+            entry_created_time = strptime(
+                row['created_time'], "%Y-%m-%dT%H:%M:%S+0000")
             logging.info(last_synced_time)
             logging.info(entry_created_time)
             logging.info(last_synced_time >= entry_created_time)
             if last_synced_time >= entry_created_time:
                 logging.info('skip')
                 continue
-
-            NewsFeedMessage +=  '<hr />' +row["message"]
+            NewsFeedMessage += '<hr />' + row["message"]
             feed = Feed(
-                id = row.get('id') or '',
-                message = row.get('message') or '',
-                full_picture= row.get('full_picture') or '', 
+                id=row.get('id') or '',
+                message=row.get('message') or '',
+                full_picture=row.get('full_picture') or '',
                 created_time=row['created_time'],
                 updated_time=row['updated_time'],
-                link = row.get('link') or '')
+                link=row.get('link') or '')
             feed.put()
             max_created_time = max(max_created_time, entry_created_time)
-
         if config:
             config.value = strftime("%Y-%m-%dT%H:%M:%S+0000", max_created_time)
             config.put()
         else:
-            Config(key='last_synced_time', value=strftime("%Y-%m-%dT%H:%M:%S+0000", max_created_time)).put()
+            Config(key='last_synced_time', value=strftime(
+                "%Y-%m-%dT%H:%M:%S+0000", max_created_time)).put()
+        self.response.write(NewsFeedMessage + '<hr >')
 
 
-        self.response.write(NewsFeedMessage+'<hr >')
+class AccessTokenHandler(BaseHandler):
+
+    def get(self):
+        self.response.write("access token refresh")
+
+        graph = Graph()
+        if graph.refreshToken(self):
+            self.response.write("<strong>access token issue</strong>")
+        else:
+            self.response.write("<strong>access token fail</strong>")    
+
+        
 
 class TestHandler(BaseHandler):
+
     def get(self):
         from time import strptime
-        a = strptime('2002', '%Y');
+        a = strptime('2002', '%Y')
         b = strptime('2011', '%Y')
-        logging.info(max(a,b))
-        return;
+        logging.info(max(a, b))
+        return
         config = Config.query().get()
         if config:
             logging.info(config)
         else:
             Config(synced_time='8888').put()
 
-def safe_str(obj):
-    """ return the byte string representation of obj """
-    try:
-        return str(obj)
-    except UnicodeEncodeError:
-        # obj is unicode
-        return unicode(obj).encode('unicode_escape')
-
-
-def set_cookie(response, name, value, domain=None, path="/",expires=None):
-    """Generates and signs a cookie for the give name/value
-    승인된 쿠키를 생
-    """
-    timestamp = str(int(time.time()))
-    value = base64.b64encode(value)
-    signature = cookie_signature(value, timestamp)
-    cookie = Cookie.BaseCookie()
-    cookie[name] = "|".join([value, timestamp, signature])
-    cookie[name]["path"] = path
-    if domain:
-        cookie[name]["domain"] = domain
-    if expires:
-        cookie[name]["expires"] = email.utils.formatdate(
-            expires, localtime=False, usegmt=True)
-    header_value = cookie.output()[12:]
-    response.headers.add_header("Set-Cookie", header_value)
-
-
-def parse_cookie_simple(value):
-    if not value:
-        return None
-
-    return base64.b64decode(value)
-
-
-def parse_cookie(value):
-    """Parses and verifies a cookie value from set_cookie"""
-    if not value:
-        return None
-    parts = value.split("|")
-    if len(parts) != 3:
-        return None
-    if cookie_signature(parts[0], parts[1]) != parts[2]:
-        logging.warning("Invalid cookie signature %r", value)
-        return None
-    timestamp = int(parts[1])
-    if timestamp < time.time() - 30 * 86400:
-        logging.warning("Expired cookie %r", value)
-        return None
-    try:
-        return base64.b64decode(parts[0]).strip()
-    except:
-        return None
-
-
-def cookie_signature(*parts):
-    """Generates a cookie signature.
-
-    We use the Facebook app secret since it is different for every app (so
-    people using this example don't accidentally all use the same secret).
-    """
-    hash = hmac.new(FACEBOOK_SECRET, digestmod=hashlib.sha1)
-    for part in parts:
-        hash.update(part)
-    return hash.hexdigest()
-
 
 app = webapp2.WSGIApplication(
-    [('/', HomeHandler), ('/auth/logout', LogoutHandler), ("/auth/login", LoginHandler), ('/groups', GroupsFqlHandler), ('/groups_api', GroupsGraphApiHandler), ('/t', TestHandler)],
-    debug=True,
-    config=config
+    [('/', HomeHandler), ('/auth/logout', LogoutHandler), ("/auth/login", LoginHandler),
+     ('/groups_api', GroupsGraphApiHandler), ('/t', TestHandler), ('/refresh_token', AccessTokenHandler)],
+    debug=True
 )
